1c8a248151ffee3fc82fd8a2e23b9b6a9efc64eb
Yurii Rybachok <drummer2009@ukr.net>
2023-04-23 13:12:45 +0300

    comments & likes migrations

--- database/migrations/2023_04_23_094633_create_comments_table.php
+++ database/migrations/2023_04_23_094633_create_comments_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->text('text');
+
             $table->timestamps();
         });
     }
